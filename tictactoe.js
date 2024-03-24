4205d7ecbf779d19cf7368ce1ea53b2fb806bfc6
AmrHishamFarouk <hishamamr121@gmail.com>
2024-03-24 23:53:10 +0200

    Add files via upload

--- tictactoe.js
+++ tictactoe.js
@@ -1,19 +1,60 @@
-function Gameboard (){
-    let gameboard = [,,,,,,,,];
+let Gameboard = (() => {
+    let gameboard = ['c','x','v','','','','','',''];
+
+    let display = () =>{
+        let blocks = document.querySelectorAll('.part');
+        blocks.forEach((e,index) =>{
+            e.textContent = gameboard[index];
+            console.log(e);
+        })
+    }
+
+    return{gameboard,display};
+})();
+
+
+
+let createplayer = (name,mark) =>{
+    return {name,mark};
 }
 
-function createplayer (player,symbol){
-    let name = player;
-    let score = 0;
-    let givescore = () => score++;
-    let getscore = () => score;
-    return {name,symbol,givescore,getscore};
-}
+
+let Game =(() =>{
+    let players = [];
+    let currentplayer = 0;
+    let gameover = false;
+    let start = () =>{
+        createplayer(document.getElementsByName('player1').value,'X')
+        createplayer(document.getElementsByName('player2').value,'O')
+        currentplayer = 0;
+        gameover = false;
+        Gameboard.display();
+    }
+
+    return{start};
+    
+})();
+
+let startbtn = document.querySelector('.startbtn');
+startbtn.addEventListener('click',() =>{
+    Game.start();
+})
 
 
 
 
 
-const amr = createplayer('amr','x');
-amr.givescore();
- console.log(amr);
+
+
+
+// function createplayer (player,symbol){
+//     let name = player;
+//     let score = 0;
+//     let givescore = () => score++;
+//     let getscore = () => score;
+//     return {name,symbol,givescore,getscore};
+// }
+
+// const amr = createplayer('amr','x');
+// amr.givescore();
+//  console.log(amr);
