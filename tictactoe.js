ff267343d999d5c21b895e60236373c06590eed2
AmrHishamFarouk <hishamamr121@gmail.com>
2024-03-31 21:29:39 +0200

    Add files via upload

--- tictactoe.js
+++ tictactoe.js
@@ -32,19 +32,23 @@ let Gameboard = (() => {
                 Game.nextTurn();
         }
     }
+
     const clear = () =>{
         gameboard = ['','','','','','','','',''];
     }
 
     const check =() =>{
     //  winSituations = [  [0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]   ];
+    let gameover = false;
         winSituations.forEach((e)=>{
-            let places[a,b,c] = e;
-            if(places[a]&&places[a]==places[b] && places[b]==places[c]){
-                return true;
+            let places = e;
+            if(gameboard[places[0]]==gameboard[places[1]] && gameboard[places[1]]==gameboard[places[2]]){
+                if(gameboard[places[0]] != ''){
+                    gameover = true;
+                }
             }
         })
-
+        return gameover;
     }
 
 
@@ -66,13 +70,13 @@ let Game =(() =>{
     const start = () =>{
         Gameboard.clear();
         players = [
-            createplayer(document.getElementsByName('player1').value,'X'),
-            createplayer(document.getElementsByName('player2').value,'O')
+            createplayer(document.querySelector('#player1').value,'X'),
+            createplayer(document.querySelector('#player2').value,'O')
         ];
         currentplayer = 0;
         gameover = false;
         Gameboard.display();
-        massage.textContent = players[currentplayer].name;
+        massage.textContent = players[currentplayer].name + ' turn';
     }
     
     const boxChoosed = (index) => {
@@ -80,12 +84,15 @@ let Game =(() =>{
         if(gameover == false){
             Gameboard.upgrade(index,currentplayer);
             gameover = Gameboard.check();
+            if(gameover == true){
+                massage.textContent = players[currentplayer].name + ' Won';
+            }
         }
     }
 
     const nextTurn = () =>{
         currentplayer = currentplayer == 0 ? 1 : 0;
-        massage.textContent = players[currentplayer].name;
+        massage.textContent = players[currentplayer].name + ' turn';
     }
     
     return{start,boxChoosed,nextTurn};
@@ -97,28 +104,3 @@ startbtn.addEventListener('click',() =>{
     Game.start();
 })
 
-
-
-
-
-
-
-
-        // for(let j=0; j<8; j++){
-        //     winX = 0;
-        //     winO = 0;
-        //     for(let i = 0; i<3; i++){
-        //         if(gameboard[winSituations[j][i]] == 'X'){
-        //             winX++ 
-        //         }
-        //         else if(gameboard[winSituations[j][i]] == 'O'){
-        //             winO++ 
-        //         }
-        //     }
-        //     if(winX == 3 || winO == 3) return true;
-        //     else{
-        //         winX = 0;
-        //         winO = 0; 
-        //         return false;
-        //     }
-        // }
